d7c34fcd343c6619d309162c766726d38bca53de
tombch <t.brier@outlook.com>
2025-02-03 21:17:48 +0000

    Mobile friendliness

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,13 @@ function Logos() {
   return (
     <Container>
       <Row className="align-items-center">
-        <Col>
+        <Col xs={6} md={4}>
           <Image src={UKHSALogo} alt="UKHSA Logo" fluid />
         </Col>
-        <Col>
+        <Col xs={6} md={4}>
           <Image src={UOBLogo} alt="University of Birmingham Logo" fluid />
         </Col>
-        <Col>
+        <Col xs={6} md={4}>
           <Image src={UOELogo} alt="University of Edinburgh Logo" fluid />
         </Col>
       </Row>
@@ -66,97 +66,107 @@ function PressRelease(props: { date: string; title: string; url: string }) {
 
 function App() {
   return (
-    <Container className="mSCAPE py-5 w-50">
-      <Stack gap={3}>
-        <Logos />
-        <Title />
-        <Section title="Overview">
-          <p>
-            The Metagenomics Surveillance Collaboration and Analysis Programme
-            (mSCAPE), is one of the key initiatives underpinning the delivery of
-            the UKHSA{" "}
-            <a href="https://assets.publishing.service.gov.uk/media/65aff68ef2718c000dfb1bd8/Pathogen_Genomics_Strategy_2024.pdf">
-              Pathogen Genomics Strategy
-            </a>
-            , and has been in development since March 2022.
-          </p>
-          <p>
-            Led by UKHSA, it is a collaborative initiative involving a
-            consortium of NHS and academic partners including the University of
-            Birmingham, University of Edinburgh, and the NHS genomic network of
-            excellence for Severe presentation of infectious disease, led by
-            Guys and St. Thomas' NHS Trust.
-          </p>
-          <p>
-            mSCAPE is a pilot surveillance network trialling the use of
-            metagenomic data for public health surveillance and pathogen
-            analysis. It takes anonymous pathogen data from multiple labs that
-            are using metagenomics for diagnosis - including those in the NHS -
-            and analyses it at a national level to monitor trends, epidemiology
-            and pathogen emergence at speed.
-          </p>
-          <p>
-            Pathogen-agnostic clinical surveillance can improve biosecurity
-            through the detection of novel, emerging and unexpected infection
-            threats. Whilst there are numerous examples of pathogen detection
-            and, to a lesser extent, discovery using pathogen-agnostic
-            techniques such as metagenomic sequencing, at present there is no
-            systematic surveillance based on this approach.
-          </p>
-          <p>
-            The principal investigators are Dr Meera Chand, UKHSA Deputy
-            director for emerging infections and clinical lead for the UKHSA
-            genomics programme, and Professor Nicholas Loman from the University
-            of Birmingham, academic Lead for the CLIMB-BIG-DATA project.
-          </p>
-        </Section>
-        <Section title="Objectives">
-          <p>By the end of the pilot, mSCAPE will have:</p>
-          <ListGroup numbered>
-            <Objective>
-              <b>Established a data sharing platform</b> (mSCAPE.CLIMB) on
-              MRC-CLIMB Data Infrastructure with safe anonymisation, information
-              governance and data management suitable for metagenomic data,
-              including any trusted research environment function.
-            </Objective>
-            <Objective>
-              <b>Trialled the feasibility of a networked surveillance system</b>{" "}
-              for generating and monitoring timely and actionable pathogen
-              agnostic data.
-            </Objective>
-            <Objective>
-              <b>Evaluated the performance characteristics</b> of such a system
-              by comparison to conventional detections and trends identified
-              through existing surveillance.
-            </Objective>
-            <Objective>
-              <b>Exercised the system using artificial constructs</b> of novel
-              pathogens to determine sensitivity and specificity of approach on
-              a background of normal cases.
-            </Objective>
-            <Objective>
-              <b>
-                Designed sampling frameworks for pathogen-agnostic surveillance
-              </b>{" "}
-              of respiratory, emerging, and imported infections.
-            </Objective>
-          </ListGroup>
-        </Section>
-        <Section title="Press Releases">
-          <ListGroup>
-            <PressRelease
-              date="30th January 2025"
-              title="UKHSA launches new metagenomic surveillance for health security"
-              url="https://www.gov.uk/government/news/ukhsa-launches-new-metagenomic-surveillance-for-health-security"
-            />
-            <PressRelease
-              date="5th November 2024"
-              title="UK to create world-first 'early warning system' for pandemics"
-              url="https://www.gov.uk/government/news/uk-to-create-world-first-early-warning-system-for-pandemics"
-            />
-          </ListGroup>
-        </Section>
-      </Stack>
+    <Container className="mSCAPE py-5">
+      <Row className="justify-content-md-center">
+        <Col lg={9}>
+          <Stack gap={3}>
+            <Logos />
+            <Title />
+            <Section title="Overview">
+              <p>
+                The Metagenomics Surveillance Collaboration and Analysis
+                Programme (mSCAPE), is one of the key initiatives underpinning
+                the delivery of the UKHSA{" "}
+                <a href="https://assets.publishing.service.gov.uk/media/65aff68ef2718c000dfb1bd8/Pathogen_Genomics_Strategy_2024.pdf">
+                  Pathogen Genomics Strategy
+                </a>
+                , and has been in development since March 2022.
+              </p>
+              <p>
+                Led by UKHSA, it is a collaborative initiative involving a
+                consortium of NHS and academic partners including the University
+                of Birmingham, University of Edinburgh, and the NHS genomic
+                network of excellence for Severe presentation of infectious
+                disease, led by Guys and St. Thomas' NHS Trust.
+              </p>
+              <p>
+                mSCAPE is a pilot surveillance network trialling the use of
+                metagenomic data for public health surveillance and pathogen
+                analysis. It takes anonymous pathogen data from multiple labs
+                that are using metagenomics for diagnosis - including those in
+                the NHS - and analyses it at a national level to monitor trends,
+                epidemiology and pathogen emergence at speed.
+              </p>
+              <p>
+                Pathogen-agnostic clinical surveillance can improve biosecurity
+                through the detection of novel, emerging and unexpected
+                infection threats. Whilst there are numerous examples of
+                pathogen detection and, to a lesser extent, discovery using
+                pathogen-agnostic techniques such as metagenomic sequencing, at
+                present there is no systematic surveillance based on this
+                approach.
+              </p>
+              <p>
+                The principal investigators are Dr Meera Chand, UKHSA Deputy
+                director for emerging infections and clinical lead for the UKHSA
+                genomics programme, and Professor Nicholas Loman from the
+                University of Birmingham, academic Lead for the CLIMB-BIG-DATA
+                project.
+              </p>
+            </Section>
+            <Section title="Objectives">
+              <p>By the end of the pilot, mSCAPE will have:</p>
+              <ListGroup numbered>
+                <Objective>
+                  <b>Established a data sharing platform</b> (mSCAPE.CLIMB) on
+                  MRC-CLIMB Data Infrastructure with safe anonymisation,
+                  information governance and data management suitable for
+                  metagenomic data, including any trusted research environment
+                  function.
+                </Objective>
+                <Objective>
+                  <b>
+                    Trialled the feasibility of a networked surveillance system
+                  </b>{" "}
+                  for generating and monitoring timely and actionable pathogen
+                  agnostic data.
+                </Objective>
+                <Objective>
+                  <b>Evaluated the performance characteristics</b> of such a
+                  system by comparison to conventional detections and trends
+                  identified through existing surveillance.
+                </Objective>
+                <Objective>
+                  <b>Exercised the system using artificial constructs</b> of
+                  novel pathogens to determine sensitivity and specificity of
+                  approach on a background of normal cases.
+                </Objective>
+                <Objective>
+                  <b>
+                    Designed sampling frameworks for pathogen-agnostic
+                    surveillance
+                  </b>{" "}
+                  of respiratory, emerging, and imported infections.
+                </Objective>
+              </ListGroup>
+            </Section>
+            <Section title="Press Releases">
+              <ListGroup>
+                <PressRelease
+                  date="30th January 2025"
+                  title="UKHSA launches new metagenomic surveillance for health security"
+                  url="https://www.gov.uk/government/news/ukhsa-launches-new-metagenomic-surveillance-for-health-security"
+                />
+                <PressRelease
+                  date="5th November 2024"
+                  title="UK to create world-first 'early warning system' for pandemics"
+                  url="https://www.gov.uk/government/news/uk-to-create-world-first-early-warning-system-for-pandemics"
+                />
+              </ListGroup>
+            </Section>
+          </Stack>
+        </Col>
+      </Row>
     </Container>
   );
 }
